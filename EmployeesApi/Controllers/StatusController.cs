0db4868bc0578984501466d1853610d1e3d5164f
D'Mitri Thompson <dmitrithompson1@yahoo.com>
2021-09-07 16:18:34 -0400

    more stuff

---
+++ EmployeesApi/Controllers/StatusController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesApi.Controllers
+{
+    public class StatusController : ControllerBase
+    {
+        // GET /Status -> 200
+        [HttpGet("status")]
+        public ActionResult GetTheStatus()
+        {
+            var response = new GetStatusResponse
+            {
+                Message = "The server is running great!",
+                WhenCheckecked = DateTime.Now
+            };
+            return Ok(response);
+        }
+    }
+
+    public class GetStatusResponse
+    {
+        public string Message { get; set;}
+        public DateTime WhenCheckecked { get; set; }
+    }
+}
