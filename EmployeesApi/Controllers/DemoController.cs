fd28750ebfdec94782d50b3d8c00648290ada57c
D'Mitri Thompson <dmitrithompson1@yahoo.com>
2021-09-07 16:46:27 -0400

    Done with first day

---
+++ EmployeesApi/Controllers/DemoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesApi.Controllers
+{
+    public class DemoController : ControllerBase
+    {
+        // In the Url itself (RouteData)
+        // GET /products/83989389
+
+        [HttpGet("products/{sku:int}")]
+        public ActionResult LookUpProduct(int sku)
+        {
+            return Ok($"Here is product {sku}");
+        }
+    }
+}
